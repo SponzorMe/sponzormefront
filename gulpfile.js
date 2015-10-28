54cd1913109a6007d85056c8cda5b66b4c20e8eb
Sebastian Gomez <seagomezar@gmail.com>
2015-10-28 16:07:15 -0300

    All gulp processes done, distribution done, serve dev done

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,103 @@ var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
 	concatCss = require('gulp-concat-css'),
 	concatJs = require('gulp-concat'),
-  notify = require('gulp-notify'),
+  	notify = require('gulp-notify'),
 	uglify = require('gulp-uglify'),
+	minifyHTML = require('gulp-minify-html'),
+	clean = require('gulp-rimraf'),
+	imagemin = require('gulp-imagemin'),
+	serve = require('gulp-serve'),
 	ngAnnotate = require('gulp-ng-annotate');
+var browserSync = require('browser-sync').create();
 
-gulp.task('css', function ()
-{
-  gulp.src('app/styles/*.css')
-    .pipe(concatCss("main.css"))
-    .pipe(minifyCSS({keepBreaks:false}))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(notify("Ha finalizado la task css!"));
-});
-
-gulp.task('js', function()
-{
-  gulp.src(['app/scripts/app.js','app/scripts/**/*.js','app/scripts/**/**/*.js'])
-		.pipe(ngAnnotate())
-  	.pipe(concatJs('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-    .pipe(notify("Ha finalizado la task js!"));
-});
-
-gulp.task('html', function()
+gulp.task('main', function()
 {
   gulp.src(['app/index.html'])
 	.pipe(usemin({
 			css: [minifyCSS(), 'concat'],
-			js: [ngAnnotate(), uglify(), 'concat']
+			js: [ngAnnotate(), 'concat']
 	}))
 	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('views', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+ 
+  return gulp.src(['app/views/*.html','app/views/**/*.html',,'app/views/**/**/*.html'])
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('dist/views'));
+});
+
+gulp.task('imagenes', function () {
+    return gulp.src(['app/images/*.*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('extras', function () {
+    return gulp.src(['app/favicon.ico','app/robots.txt'])
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('langs', function () {
+    return gulp.src(['app/langs/*.json'])
+        .pipe(gulp.dest('dist/langs/'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src(['app/fonts/*.*','app/fonts/**/*.*'])
+        .pipe(gulp.dest('dist/fonts/'));
+});
+
+
+gulp.task('clean', function() {
+    return gulp.src(['dist/**/*.*','dist/**/**/*.*','dist/*.*','dist/*'], { read: false })
+           .pipe(clean({ force: true }));
+});
+gulp.task('build', ['main','views','imagenes','langs','fonts','extras']);
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
+});
+
+gulp.task('serve', function (){
+  browserSync.init({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/*.html',
+    'app/views/*.html',
+    'app/views/**/*.html',
+    'langs/*.json',
+    'app/scripts/*.js',
+    'app/scripts/**/*.js',
+    'app/scripts/**/**/*.js',
+    'bower_components/*',
+  ]).on('change', browserSync.reload);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json');
+});
+
+gulp.task('serve:dist', function (){
+  gulp.start('default');
+  browserSync.init({
+    notify: false,
+    port: 3000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
 });
