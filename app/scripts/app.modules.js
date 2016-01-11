44c41d2e30313dbf55bd440a8f61794c55cedaa8
Sebastian Gomez <seagomezar@gmail.com>
2016-01-11 12:11:18 -0300

    Bad thing

---
+++ app/scripts/app.modules.js
@@ -0,0 +1,28 @@
+/**
+* @File the start the app
+*
+* @author Sebastian Gomez
+* @version 0.1
+*/
+(function() {
+  'use strict';
+  angular.module('sponzorme', [
+    'pascalprecht.translate',
+    'ngResource',
+    'ngRoute',
+    'ngDialog',
+    'base64',
+    'ngCookies',
+    'ngStorage',
+    'ui.bootstrap',
+    'google.places',
+    'angularSpinner',
+    'naif.base64',
+    'angularUtils.directives.dirPagination',
+    'ui.bootstrap.datetimepicker',
+    'firebase',
+    'textAngular',
+    'angular-input-stars',
+    'luegg.directives'
+  ]);
+})();
