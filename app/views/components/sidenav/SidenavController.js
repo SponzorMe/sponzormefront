031eb2fe67fc17fb18e012a8d8dd7a9e6d22a99f
Diego Coy <eltu.khan@gmail.com>
2016-01-28 18:01:24 -0500

    add navbar, sidenav and footer components comment all no needed style modules

---
+++ app/views/components/sidenav/SidenavController.js
@@ -0,0 +1,50 @@
+'use strict';
+(function() {
+  function SidenavController($scope, $mdSidenav, $route) {
+    //mock starts
+    $scope.route = $route.current.loadedTemplateUrl;
+    console.log('esta es la ruta: ', $scope.route);
+    $scope.statesSponzors = {
+        'dashboard': 'views/sponzors/dashboard/main.html',
+        'sponzoring': {
+          'sponzoring': 'views/sponzors/dashboard/sponzorships.html',
+          'sponzoringId': 'views/sponzors/dashboard/sponzorshipsId.html'
+        },
+        'following': 'views/sponzors/dashboard/following.html',
+        'tasks': '',
+        'notifications': {
+          'notifications': 'views/sponzors/notifications/notifications.html',
+          'notificationId': 'views/sponzors/notifications/notification_detail.html',
+          'notificationReply': 'views/sponzors/notifications/notification_reply.html',
+        },
+        'settings': {
+          'profile': 'views/sponzors/dashboard/settings_profile.html',
+          'preferences': 'views/sponzors/dashboard/settings_preferences.html'
+        }
+    };
+
+    $scope.getRoute = function(route1, route2, route3) {
+      if($scope.route === route1 || $scope.route === route2 || $scope.route === route3) {
+        return true;
+      }
+    }
+
+    $scope.openMenu = function($mdOpenMenu, $event) {
+      $scope.originatorEv = $event;
+      $mdOpenMenu($event);
+    };
+    
+    $scope.openSidenavLeft = function(){
+      $mdSidenav('left').toggle();
+    };
+
+    $scope.isOpenLeft = function () {
+      var isOpen = true;
+      return isOpen = $mdSidenav('left').isOpen();
+    };
+
+}
+  angular.module('sponzorme')
+    .controller('SidenavController', SidenavController);
+
+})();
