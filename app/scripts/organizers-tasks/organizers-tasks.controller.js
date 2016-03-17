31175496868eca0e60645d4490deaf2c558d7236
Sebastian Gomez <seagomezar@gmail.com>
2016-03-17 17:52:19 -0300

    Working on tasks view

--- app/scripts/organizers-tasks/organizers-tasks.controller.js
+++ app/scripts/organizers-tasks/organizers-tasks.controller.js
@@ -7,13 +7,20 @@
       vm.regenerateTasks = function () {
         vm.tasks = [];
         for (var i = 0; i < vm.user.sponzorships_like_organizer.length; i++) {
-          for (var j = 0; j < vm.user.sponzorships_like_organizer[i].task_sponzor.length; j++) {
-            var currentTask = vm.user.sponzorships_like_organizer[i].task_sponzor[j];
-            currentTask.event = vm.user.sponzorships_like_organizer[i].event;
-            vm.tasks.push(currentTask);
+          for (var j = 0; j < vm.user.events.length; j++) {
+            if(vm.user.sponzorships_like_organizer[i].event.id == vm.user.events[j].id){
+              vm.user.sponzorships_like_organizer[i].event = vm.user.events[j];
+              console.log(vm.user.sponzorships_like_organizer[i]);
+            }
           }
         }
       };
+      vm.activePerk = function (p, e) {
+        for(var i=0; i< e.perks.length; i++){
+          e.perks[i].active = false;
+        }
+        p.active = true;
+      }
       vm.regenerateTasks();
       vm.currentTaskIndex = $routeParams.id;
     }
