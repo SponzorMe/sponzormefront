8a1579a4dca130fa14fa3e237070f7b9741f6199
Sebastian Gomez <seagomezar@gmail.com>
2016-03-21 15:34:42 -0300

    With Tasks completion complete

--- app/scripts/organizers-tasks/organizers-tasks.controller.js
+++ app/scripts/organizers-tasks/organizers-tasks.controller.js
@@ -1,17 +1,37 @@
 (function() {
   'use strict';
-  function OrganizersTasksController($scope, $localStorage, $rootScope, $routeParams) {
+
+  function OrganizersTasksController($scope, $localStorage, $rootScope, $routeParams, taskSponzorRequest) {
     if ($rootScope.userValidation('0')) {
       var vm = this;
       vm.user = JSON.parse($localStorage.user);
-      vm.activePerk = function (p, e) {
-        for(var i=0; i< e.perks.length; i++){
+      vm.activePerk = function(p, e) {
+        for (var i = 0; i < e.perks.length; i++) {
           e.perks[i].active = false;
         }
         p.active = true;
         vm.showHeader = true;
       }
-      console.log(vm.user.eventTasks);
+      vm.changeStatus = function(t) {
+        t.loading = true;
+        var savedStatus = t.status;
+        if (t.status === '1' || t.status === 1) {
+          t.status = 0;
+        } else {
+          t.status = 1;
+        }
+        var data = {
+          status: t.status
+        };
+        taskSponzorRequest.editTaskSponzorPatch(t.id, data).then(function successCallBack(response) {
+          t.loading = false;
+          $localStorage.user = JSON.stringify($scope.user);
+        }, function errorCallback() {
+          t.status = savedStatus;
+          t.loading = false;
+          dialogRequest.showDialog('error', 'errorUpdatingTaskStatus', false);
+        });
+      };
       vm.currentTaskIndex = $routeParams.id;
     }
   }
