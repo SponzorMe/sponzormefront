970d058c5e5e2f3a40a620356fcf9f9f9f5e64b1
Sebastian Gomez <seagomezar@gmail.com>
2016-03-28 13:10:51 -0300

    adding tasks, adding sponzorship types

--- app/scripts/organizers-event-add/organizers-event-add.controller.js
+++ app/scripts/organizers-event-add/organizers-event-add.controller.js
@@ -1,40 +1,32 @@
-'use strict';
 (function() {
+  'use strict';
   function OrganizersEventAddController($scope, $mdDialog, $translate, $localStorage, eventRequest, $rootScope, $routeParams, eventbriteRequest, dialogRequest, eventTypeRequest, categoryRequest) {
-
     if ($rootScope.userValidation('0')) {
       var vm = this;
-      vm.setEventData = function() {
-          if (!$localStorage.eventTypes) {
-            eventTypeRequest.allEventTypes().then(function successCallback1(response) {
-              $localStorage.eventTypes = JSON.stringify(response.data.eventTypes);
-              vm.eventTypes = response.data.eventTypes;
-            });
-          } else {
-            vm.eventTypes = JSON.parse($localStorage.eventTypes);
-          }
-          if (!$localStorage.categories) {
-            categoryRequest.allCategories().then(function successCallback2(response) {
-              $localStorage.categories = JSON.stringify(response.data.categories);
-              vm.categories = response.data.categories;
-            });
-          } else {
-            vm.categories = JSON.parse($localStorage.categories);
-          }
-        }
-        //mock starts
-      vm.date = new Date();
-      vm.eventProviderSelected = '';
-
       vm.event = {
         sponzorshipTypes: []
       };
-      vm.setEventData();
-      //End vars Initialization
-      //
+      vm.setEventData = function() {
+        if (!$localStorage.eventTypes) {
+          eventTypeRequest.allEventTypes().then(function successCallback1(response) {
+            $localStorage.eventTypes = JSON.stringify(response.data.eventTypes);
+            vm.eventTypes = response.data.eventTypes;
+          });
+        } else {
+          vm.eventTypes = JSON.parse($localStorage.eventTypes);
+        }
+        if (!$localStorage.categories) {
+          categoryRequest.allCategories().then(function successCallback2(response) {
+            $localStorage.categories = JSON.stringify(response.data.categories);
+            vm.categories = response.data.categories;
+          });
+        } else {
+          vm.categories = JSON.parse($localStorage.categories);
+        }
+      };
 
-      vm.aa = function() {
-        console.log("hola");
+      vm.showSponzorshipType = function(s) {
+        s.show = !s.show;
       };
 
       //This function creates an event
@@ -99,172 +91,45 @@
           vm.createNewEvent();
         }
       };
-      console.log("estoy");
       vm.addSponzorshipTypeForm = function() {
-        console.log("test");
-        var parentEl = angular.element(document.body);
         $mdDialog.show({
-          parent: parentEl,
           clickOutsideToClose: true,
-          templateUrl: 'views/components/dialogs/sponzorshipTypeForm.html',
-          bindToController: true,
-          scope: $scope
+          templateUrl: 'scripts/organizers-event-add/sponzorshipTypeForm.html',
+          controller: function($scope){
+            $scope.addSponzorshipType = function() {
+              vm.event.sponzorshipTypes.push({
+                kind: $scope.newSponzorshipType.kind,
+                usd: $scope.newSponzorshipType.usd,
+                total_quantity: $scope.newSponzorshipType.totalQuantity,
+                reserved_quantity: $scope.newSponzorshipType.reservedQuantity
+              });
+              $mdDialog.hide();
+            };
+          }
+        });
+      };
+
+      vm.addTaskForm = function($index){
+        $mdDialog.show({
+          clickOutsideToClose: true,
+          templateUrl: 'scripts/organizers-event-add/taskForm.html',
+          controller: function($scope){
+            $scope.addTask = function() {
+              vm.event.sponzorshipTypes[$index].perkTasks.push({
+                title: $scope.newTask.title,
+                description: $scope.newTask.title
+              });
+              $mdDialog.hide();
+            };
+          }
         });
       };
       //this function adds a SponzorshipType to the new event form
-      $scope.addSponzorshipType = function() {
-        vm.event.sponzorshipTypes.push({
-          kind: $scope.newSponzorshipType.kind,
-          usd: $scope.newSponzorshipType.usd,
-          total_quantity: $scope.newSponzorshipType.totalQuantity,
-          reserved_quantity: $scope.newSponzorshipType.reservedQuantity
-        });
-        $mdDialog.hide();
-      };
+
       //this function removes a SponzorshipType to the new event form
       vm.removeSponzorshipType = function(index) {
-        vm.event.perks.splice(index, 1);
+        vm.event.sponzorshipTypes.splice(index, 1);
       };
-/*
-      if ($routeParams.eventBriteCode) {
-        eventbriteRequest.getEventbriteAuth($routeParams.eventBriteCode).success(function(data) {
-          var response = JSON.parse(jsonize(data.response));
-          if (response.error) {
-            vm.loadingGetToken = false;
-            vm.reconnectEventbrite = true;
-            vm.conectionDone = false;
-          } else {
-            $localStorage.eventBriteBeared = response.access_token;
-            vm.connectEventbrite();
-          }
-        });
-      } else if ($routeParams.meetupCode) {
-        eventbriteRequest.getMeetupAuth($routeParams.meetupCode).success(function(data) {
-          var response = JSON.parse(jsonize(data.response));
-          if (response.error) {
-            vm.meetupLoadingGetToken = false;
-            vm.reconnectMeetup = true;
-            vm.meetupConectionDone = false;
-          } else {
-            $localStorage.meetupBeared = response.access_token;
-            vm.connectMeetup();
-          }
-        });
-      }
-      vm.connectMeetup = function() {
-        vm.meetupLoadingGetToken = true;
-        vm.loadingGetMeetupEvents = false;
-        ngDialog.open({
-          template: 'views/templates/importMeetupDialog.html',
-          showClose: false,
-          scope: $scope
-        });
-        if ($localStorage.meetupBeared) {
-          vm.meetupLoadingGetToken = false;
-          vm.loadingGetMeetupEvents = true;
-          vm.meetupConectionDone = true;
-          vm.getMeetupGroups($localStorage.meetupBeared);
-        } else {
-          vm.meetupLoadingGetToken = false;
-          vm.loadingGetMeetupEvents = false;
-          vm.meetupConectionDone = false;
-          eventbriteRequest.getMeetupAuth($routeParams.meetupBeared).success(function(data) {
-            var response = JSON.parse(jsonize(data.response));
-            if (response.error) {
-              vm.meetupLoadingGetToken = false;
-              vm.reconnectMeetup = true;
-              vm.meetupConectionDone = false;
-            } else {
-              $localStorage.meetupBeared = response.access_token;
-              vm.meetupLoadingGetToken = false;
-              vm.loadingGetMeetupEvents = true;
-              vm.meetupConectionDone = true;
-              vm.getMeetupGroups(response.access_token);
-            }
-          });
-        }
-      };
-      vm.EVENTBRITEAPYKEY = $rootScope.getConstants().EVENTBRITEAPYKEY;
-      vm.getEventbriteEvents = function(accessToken) {
-        eventbriteRequest.getEventbriteEvents(accessToken)
-          .success(function(data, head) {
-            vm.loadingGetEvents = false;
-            vm.evenbriteEvents = data.events;
-          }).error(function(data) {
-            vm.loadingGetEvents = false;
-            vm.errorGettingEvents = true;
-            vm.evenbriteEvents = false;
-          });
-      };
-      vm.MEETUPAPIKEY = $rootScope.getConstants().MEETUPAPIKEY;
-      vm.MEETUPREDIRECTURL = $rootScope.getConstants().MEETUPREDIRECTURL;
-      vm.getMeetupGroups = function(accessToken) {
-        eventbriteRequest.getMeetupGroups(accessToken)
-          .success(function(data) {
-            vm.loadingGetMeetupEvents = false;
-            vm.meetupEvents = JSON.parse(data.response);
-          }).error(function(data) {
-            vm.loadingGetMeetupEvents = false;
-            vm.errorGettingGroups = true;
-            vm.meetupEvents = false;
-          });
-      };
-      vm.connectEventbrite = function() {
-        vm.loadingGetToken = true;
-        vm.loadingGetEvents = false;
-        ngDialog.open({
-          template: 'views/templates/importEventbriteDialog.html',
-          showClose: false,
-          scope: $scope
-        });
-        if ($localStorage.eventBriteBeared) {
-          vm.loadingGetToken = false;
-          vm.loadingGetEvents = true;
-          vm.conectionDone = true;
-          vm.getEventbriteEvents($localStorage.eventBriteBeared);
-        } else {
-          vm.loadingGetToken = false;
-          vm.loadingGetEvents = false;
-          vm.conectionDone = false;
-          eventbriteRequest.getEventbriteAuth($routeParams.eventBriteCode).success(function(data) {
-            var response = JSON.parse(jsonize(data.response));
-            if (response.error) {
-              vm.loadingGetToken = false;
-              vm.reconnectEventbrite = true;
-              vm.conectionDone = false;
-            } else {
-              $localStorage.eventBriteBeared = response.access_token;
-              vm.loadingGetToken = false;
-              vm.loadingGetEvents = true;
-              vm.conectionDone = true;
-              vm.getEventbriteEvents(response.access_token);
-            }
-          });
-        }
-      };
-      vm.prefilEventForm = function(url) {
-        eventbriteRequest.getEventbriteEvent(url, $localStorage.eventBriteBeared)
-          .success(function(data) {
-            vm.event.title = data.name.text;
-            vm.event.description = data.description.html;
-            vm.event.starts = data.start.local;
-            vm.event.ends = data.end.local;
-            vm.event.privacy = 0;
-            dialogRequest.closeLoading();
-          });
-      };
-      vm.prefilEventFormMeetup = function(e) {
-        vm.event.title = e.name;
-        vm.event.description = e.description;
-        vm.event.starts = new Date(e.time);
-        var dataTime = new Date(e.time);
-        var timer = parseInt(1 * 2 * 60 * 60 * 1000);
-        var dataExpDate = new Date(dataTime.getTime() + timer);
-        vm.event.ends = new Date(dataExpDate);
-        vm.privacyevent = 0;
-        dialogRequest.closeLoading();
-      };
-*/      
     }
   }
   angular.module('sponzorme').controller('OrganizersEventAddController', OrganizersEventAddController);
