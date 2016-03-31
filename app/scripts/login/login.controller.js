3621cbde8eb5eafa8f4921ac9a9b9304fdcd9ec9
Sebastian Gomez <seagomezar@gmail.com>
2016-03-31 11:56:30 -0300

    Fixing sponzors login

--- app/scripts/login/login.controller.js
+++ app/scripts/login/login.controller.js
@@ -34,7 +34,10 @@
                 return e;
               });
             }
-            response.data.user.eventTasks = response.data.eventTasks[0];
+            if(response.data.eventTasks){
+              response.data.user.eventTasks = response.data.eventTasks[0];
+            }
+
             if(response.data.user.events){
               response.data.user.events = response.data.user.events.filter(function(e){
                 e.starts = e.starts.replace(' ', 'T');
