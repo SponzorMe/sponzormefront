8a5922a3144e370c61c4e608a90ff327ba1a3032
Sebastian Gomez A <seagomezar@MacBook-Pro-de-Sebastian.local>
2016-04-27 13:40:01 -0300

    Fixing #137, improving the filter there

--- app/organizers-main/organizers-main.controller.js
+++ app/organizers-main/organizers-main.controller.js
@@ -28,7 +28,6 @@
 
       vm.hasSponzorship = function(idEvent) {
         for (var i = 0; i < vm.user.sponzorships_like_organizer.length; i++) {
-          console.log(idEvent, vm.user.sponzorships_like_organizer[i].event.id);
           if (vm.user.sponzorships_like_organizer[i].event.id === idEvent) {
             return true;
           }
@@ -38,29 +37,22 @@
 
       vm.deleteEvent = function(eventId){
         dialogRequest.showLoading();
-        var i = 0;
-        var eventIndex = vm.user.events.filter(function(e){
-          if(e.id === eventId){
-            console.log(e, eventIndex, vm.user.events[i].id);
-            console.log(vm.hasSponzorship(vm.user.events[i].id));
-            return i;
-          }
-          else{
-            i++;
-          }
-        });
         if (vm.hasSponzorship(eventId)) {
           dialogRequest.closeLoading();
           dialogRequest.showDialog('error', 'eventDeletingEventHasSponzorship', false);
         } else {
-          console.log('antesDeBorrar', eventId);
           eventRequest.deleteEvent(eventId).then(function successCallback(response) {
-            vm.user.events.splice(eventIndex, 1);
+            var aux = vm.user.events.filter(function(e){
+              if(e.id!==eventId){
+                return e;
+              }
+            });
+            vm.user.events = [];
+            vm.user.events = aux;
             $localStorage.user = JSON.stringify(vm.user);
-            vm.user = JSON.parse($localStorage.user);
             dialogRequest.closeLoading();
             dialogRequest.showDialog('success', 'eventDeleteSuccesfully',false);
-          }, function errorCallback() {
+          }, function errorCallback(err) {
             dialogRequest.closeLoading();
             dialogRequest.showDialog('error', 'errorDeletingEvent', false);
           });
@@ -73,7 +65,6 @@
 
       vm.todayDate = new Date().getTime();
       vm.user = JSON.parse($localStorage.user);
-      console.log(vm.user);
       vm.user.balance = 0;
       angular.forEach(vm.user.sponzorships_like_organizer, function(value) {
         if (value.status === '1') {
