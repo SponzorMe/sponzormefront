864388b5399b5cd5a0e837309cc2ff47b890cc3b
Sebastian Gomez A <seagomezar@MacBook-Pro-de-Sebastian.local>
2016-05-04 19:36:05 -0300

    Closing #169

--- app/organizers-tasks/organizers-tasks.controller.js
+++ app/organizers-tasks/organizers-tasks.controller.js
@@ -11,7 +11,14 @@
         }
         p.active = true;
         vm.showHeader = true;
-      }
+      };
+      vm.user.eventTasks[0].show = true;
+      if(vm.user.eventTasks[0].perks[0].sponzor_tasks.length)
+        vm.activePerk(vm.user.eventTasks[0].perks[0],vm.user.eventTasks[0]);
+      else if(vm.user.eventTasks[0].perks[1].sponzor_tasks.length)
+        vm.activePerk(vm.user.eventTasks[0].perks[1],vm.user.eventTasks[0]);
+      else if(vm.user.eventTasks[0].perks[2].sponzor_tasks.length)
+        vm.activePerk(vm.user.eventTasks[0].perks[2],vm.user.eventTasks[0]);
       vm.changeStatus = function(t) {
         t.loading = true;
         var savedStatus = t.status;
--- dist/scripts.js
+++ dist/scripts.js
@@ -1118,6 +1118,7 @@
         });
       },
       editUserPatch: function(userId, data) {
+        console.log('data before send', data);
         var token = $localStorage.token;
         return $http({
           method: 'PATCH',
@@ -1687,22 +1688,21 @@ function sponzorsNavbar() {
   angular.module('sponzorme').controller('SponzorsPreferencesController', SponzorsPreferencesController);
 })();
 
-(function() {
+(function () {
   'use strict';
   SponzorsProfileController.$inject = ["$scope", "$translate", "userRequest", "$localStorage", "$rootScope", "loginRequest", "userInterestRequest", "dialogRequest"];
   function SponzorsProfileController($scope, $translate, userRequest, $localStorage, $rootScope, loginRequest, userInterestRequest, dialogRequest) {
     if ($rootScope.userValidation('1')) {
       var vm = this;
       vm.user = JSON.parse($localStorage.user);
-      console.log(vm.user);
-      vm.file = false;
-      vm.editAccount = function(){
+      $scope.file = false;
+      vm.editAccount = function () {
         dialogRequest.showLoading();
         if (vm.user.location !== vm.locationUser) {
           vm.user.location = vm.locationUser.formatted_address;
           vm.user.location_reference = vm.locationUser.place_id;
         }
-        if (vm.file) {
+        if ($scope.file) {
           AWS.config.update({
             accessKeyId: $rootScope.getConstants().AMAZONKEY,
             secretAccessKey: $rootScope.getConstants().AMAZONSECRET
@@ -1714,39 +1714,56 @@ function sponzorsNavbar() {
             }
           });
           // Prepend Unique String To Prevent Overwrites
-          var uniqueFileName = btoa($rootScope.uniqueString() + new Date().getTime() + $rootScope.uniqueString()).replace('=', $rootScope.uniqueString()) + '.' + $rootScope.getExtension(vm.file.name);
+          var uniqueFileName = btoa($rootScope.uniqueString() + new Date().getTime() + $rootScope.uniqueString()).replace('=', $rootScope.uniqueString()) + '.' + $rootScope.getExtension($scope.file.name);
           var params = {
             Key: uniqueFileName,
-            ContentType: vm.file.type,
-            Body: vm.file,
+            ContentType: $scope.file.type,
+            Body: $scope.file,
             ServerSideEncryption: 'AES256'
           };
-          bucket.putObject(params, function(err, data) {
+          bucket.putObject(params, function (err, data) {
             if (!err) {
               $localStorage.image = $rootScope.getConstants().AMAZONBUCKETURL + uniqueFileName;
               vm.user.image = $rootScope.getConstants().AMAZONBUCKETURL + uniqueFileName;
-              vm.user.name = vm.user.firstName +' '+ vm.user.lastName;
-              userRequest.editUserPatch($localStorage.id, vm.user).then(function successCallback(response) {
+              vm.user.name = vm.user.firstName + ' ' + vm.user.lastName;
+              var userInfo = {
+                'name': vm.user.firstName + ' ' + vm.user.lastName,
+                'image': $localStorage.image,
+                'company': vm.user.company,
+                'email': vm.user.email,
+                'location': vm.user.location,
+                'location_reference': vm.user.location_reference,
+                'phone': vm.user.phone
+              };
+              userRequest.editUserPatch($localStorage.id, userInfo).then(function successCallback(response) {
                 $localStorage.user = JSON.stringify(vm.user);
-                vm.file = false;
+                $scope.file = false;
                 dialogRequest.closeLoading();
-                dialogRequest.showDialog('success', 'dialog.accountInfoEditedSuccessfuly', false);
+                dialogRequest.showDialog('success', 'accountInfoEditedSuccessfuly', false);
               }, function errorCallback(err) {
                 dialogRequest.closeLoading();
-                dialogRequest.showDialog('error', 'dialog.errorEditingAccountInfo', false);
+                dialogRequest.showDialog('error', 'errorEditingAccountInfo', false);
               });
             }
           });
         } else {
-          vm.user.name = vm.user.firstName +' '+ vm.user.lastName;
+          var userInfo = {
+            'name': vm.user.firstName + ' ' + vm.user.lastName,
+            'image': $localStorage.image,
+            'company': vm.user.company,
+            'email': vm.user.email,
+            'location': vm.user.location,
+            'location_reference': vm.user.location_reference,
+            'phone': vm.user.phone
+          };
           userRequest.editUserPatch($localStorage.id, vm.user).then(function successCallback(response) {
             $localStorage.user = JSON.stringify(vm.user);
-            vm.file = false;
+            $scope.file = false;
             dialogRequest.closeLoading();
-            dialogRequest.showDialog('success', 'dialog.accountInfoEditedSuccessfuly', false);
+            dialogRequest.showDialog('success', 'accountInfoEditedSuccessfuly', false);
           }, function errorCallback(err) {
             dialogRequest.closeLoading();
-            dialogRequest.showDialog('error', 'dialog.errorEditingAccountInfo', false);
+            dialogRequest.showDialog('error', 'errorEditingAccountInfo', false);
           });
         }
       };
@@ -3184,21 +3201,21 @@ function organizersNotificationsNavbar() {
     angular.module('sponzorme').controller('OrganizersRateController', OrganizersRateController);
 })();
 
-(function() {
+(function () {
   'use strict';
   OrganizersProfileController.$inject = ["$scope", "$log", "$translate", "userRequest", "$localStorage", "$rootScope", "loginRequest", "userInterestRequest", "dialogRequest"];
   function OrganizersProfileController($scope, $log, $translate, userRequest, $localStorage, $rootScope, loginRequest, userInterestRequest, dialogRequest) {
     if ($rootScope.userValidation('0')) {
       var vm = this;
       vm.user = JSON.parse($localStorage.user);
-      vm.file = false;
-      vm.editAccount = function(){
+      $scope.file = false;
+      vm.editAccount = function () {
         dialogRequest.showLoading();
         if (vm.user.location !== vm.locationUser) {
           vm.user.location = vm.locationUser.formatted_address;
           vm.user.location_reference = vm.locationUser.place_id;
         }
-        if (vm.file) {
+        if ($scope.file) {
           AWS.config.update({
             accessKeyId: $rootScope.getConstants().AMAZONKEY,
             secretAccessKey: $rootScope.getConstants().AMAZONSECRET
@@ -3210,40 +3227,56 @@ function organizersNotificationsNavbar() {
             }
           });
           // Prepend Unique String To Prevent Overwrites
-          var uniqueFileName = btoa($rootScope.uniqueString() + new Date().getTime() + $rootScope.uniqueString()).replace('=', $rootScope.uniqueString()) + '.' + $rootScope.getExtension(vm.file.name);
+          var uniqueFileName = btoa($rootScope.uniqueString() + new Date().getTime() + $rootScope.uniqueString()).replace('=', $rootScope.uniqueString()) + '.' + $rootScope.getExtension($scope.file.name);
           var params = {
             Key: uniqueFileName,
-            ContentType: vm.file.type,
-            Body: vm.file,
+            ContentType: $scope.file.type,
+            Body: $scope.file,
             ServerSideEncryption: 'AES256'
           };
-          bucket.putObject(params, function(err, data) {
+          bucket.putObject(params, function (err, data) {
             if (!err) {
               $localStorage.image = $rootScope.getConstants().AMAZONBUCKETURL + uniqueFileName;
               vm.user.image = $rootScope.getConstants().AMAZONBUCKETURL + uniqueFileName;
-              vm.user.name = vm.user.firstName +' '+ vm.user.lastName;
-              $log.error(vm.user);
-              userRequest.editUserPatch($localStorage.id, vm.user).then(function successCallback(response) {
+              vm.user.name = vm.user.firstName + ' ' + vm.user.lastName;
+              var userInfo = {
+                'name': vm.user.firstName + ' ' + vm.user.lastName,
+                'image': $localStorage.image,
+                'company': vm.user.company,
+                'email': vm.user.email,
+                'location': vm.user.location,
+                'location_reference': vm.user.location_reference,
+                'phone': vm.user.phone
+              };
+              userRequest.editUserPatch($localStorage.id, userInfo).then(function successCallback(response) {
                 $localStorage.user = JSON.stringify(vm.user);
-                vm.file = false;
+                $scope.file = false;
                 dialogRequest.closeLoading();
-                dialogRequest.showDialog('success', 'dialog.accountInfoEditedSuccessfuly', false);
+                dialogRequest.showDialog('success', 'accountInfoEditedSuccessfuly', false);
               }, function errorCallback(err) {
                 dialogRequest.closeLoading();
-                dialogRequest.showDialog('error', 'dialog.errorEditingAccountInfo', false);
+                dialogRequest.showDialog('error', 'errorEditingAccountInfo', false);
               });
             }
           });
         } else {
-          vm.user.name = vm.user.firstName +' '+ vm.user.lastName;
-          userRequest.editUserPatch($localStorage.id, vm.user).then(function successCallback(response) {
+          var userInfo = {
+            'name': vm.user.firstName + ' ' + vm.user.lastName,
+            'image': $localStorage.image,
+            'company': vm.user.company,
+            'email': vm.user.email,
+            'location': vm.user.location,
+            'location_reference': vm.user.location_reference,
+            'phone': vm.user.phone
+          };
+          userRequest.editUserPatch($localStorage.id, userInfo).then(function successCallback(response) {
             $localStorage.user = JSON.stringify(vm.user);
-            vm.file = false;
+            $scope.file = false;
             dialogRequest.closeLoading();
-            dialogRequest.showDialog('success', 'dialog.accountInfoEditedSuccessfuly', false);
+            dialogRequest.showDialog('success', 'accountInfoEditedSuccessfuly', false);
           }, function errorCallback(err) {
             dialogRequest.closeLoading();
-            dialogRequest.showDialog('error', 'dialog.errorEditingAccountInfo', false);
+            dialogRequest.showDialog('error', 'errorEditingAccountInfo', false);
           });
         }
       };
